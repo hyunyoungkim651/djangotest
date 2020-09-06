5dd542b1b993db1049d599c09f217ba0ae08b668
hyunyoungkim651 <k00810111@gmail.com>
2020-09-06 20:36:16 +0900

    modified

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    return HttpResponse("Hello, world. You're at the blog2 index.")

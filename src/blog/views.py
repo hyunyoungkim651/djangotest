d7085d5aebc7f96777a91d21db5156c8ecdd1d86
hyunyoungkim651 <k00810111@gmail.com>
2020-09-07 15:51:59 +0900

    2020-09-07

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog2 index.")
+    # return HttpResponse("Hello, world. You're at the blog2 index.")
+    name = "khy"
+    return render(request, "index.html", { "name" : name })

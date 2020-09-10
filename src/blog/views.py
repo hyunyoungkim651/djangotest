3aa0fd10fd9014ac898726c43fd4d2db0ce44a73
hyunyoungkim651 <k00810111@gmail.com>
2020-09-10 16:45:59 +0900

    2020-09-10

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,19 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Article
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the blog2 index.")
-    name = "khy"
-    return render(request, "index.html", { "name" : name })
+    # name = "khy"
+    # return render(request, "index.html", { "name" : name })
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title= "hello",
+    #     contents = "this is test",
+    #     view_count = 0
+    # )
+    ctx = {
+        "article_list" : article_list
+    }
+
+    return render(request, "index.html", ctx)
